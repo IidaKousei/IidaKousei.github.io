62c8fc99b3fea1abd855f9ca267b417eb88e85d7
Iida <2002kousei@gmail.com>
2025-01-26 01:49:31 +0900

    fix: remove unused state variable in Home component
    - Removed the `isLoading` state variable from the Home component in `page.tsx`.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import Image from "next/image";
 import "./globals.css";
 
 // import { basePath } from "../../next.config" // 追加
@@ -8,8 +7,6 @@ import "./globals.css";
 
 
 export default function Home() {
-
-   const [isLoading] = useState(true);
 
   return (
     <div>
