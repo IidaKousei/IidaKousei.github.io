c0bf0f75227335e7fb23202e362fed9ca05afef1
IidaKousei <kousei.iida0504@gmail.com>
2025-06-06 20:15:59 +0900

    feat: integrate Splide.js for image carousel functionality
    - Added Splide.js as a dependency in package.json and package-lock.json
    - Updated next.config.ts to include unoptimized images setting
    - Implemented Splide component in page.tsx for image carousel
    - Declared modules for Splide.js in global.d.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 "use client";
 import { useEffect } from "react";
 import { Button, HStack, VStack, Box } from "@chakra-ui/react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import Image from "next/image";
+// デフォルトのテーマ
+import "@splidejs/react-splide/css";
+
+// または、ほかのテーマ
+import "@splidejs/react-splide/css/skyblue";
+import "@splidejs/react-splide/css/sea-green";
+
+// あるいは、コアスタイルのみ
+import "@splidejs/react-splide/css/core";
 
 export default function Home() {
   useEffect(() => {
@@ -26,6 +37,46 @@ export default function Home() {
       >
         IK Studio
       </Box>
+      <Splide
+        options={{
+          type: "loop",
+          speed: 1000,
+          perPage: 2,
+          perMove: 1,
+          autoplay: true,
+          interval: 8000,
+          pagination: true,
+          arrows: true,
+          width: "100%",
+          gap: "1rem",
+
+        }}
+      >
+        <SplideSlide>
+          <Image
+            src="https://picsum.photos/1000/400"
+            alt="画像"
+            width={1000}
+            height={400}
+          />
+        </SplideSlide>
+        <SplideSlide>
+          <Image
+            src="https://picsum.photos/1000/400"
+            alt="画像"
+            width={1000}
+            height={400}
+          />
+        </SplideSlide>
+        <SplideSlide>
+          <Image
+            src="https://picsum.photos/1000/400"
+            alt="画像"
+            width={1000}
+            height={400}
+          />
+        </SplideSlide>
+      </Splide>
     </VStack>
   );
 }
